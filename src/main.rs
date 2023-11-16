5e246ad80758330488eb4eb64dab2aaa30469493
Miracle Friday <miraclef60@gmail.com>
2023-11-16 04:14:19 +0100

    fix: fixed main functino runner

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,56 @@
 use log::Level;
-use tokio::time;
+use tokio::{io::AsyncReadExt, time};
 
-async fn run() {
+fn fib(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        n => fib(n - 1) + fib(n - 2),
+    }
+}
+
+async fn sleeper() {
     log::info!("Sleeping");
     time::sleep(time::Duration::from_secs(1)).await;
     log::info!("Awake!");
 }
 
-fn main() {
+async fn reader() {
+    log::info!("Reading some beeg data!");
+
+    let mut f = tokio::fs::File::open("beeg.csv").await.unwrap();
+    let mut contents = vec![];
+    f.read_to_end(&mut contents).await.unwrap();
+    log::info!("Reading beeg bytes {}", contents.len());
+
+    tokio::task::spawn_blocking(move || {
+        log::info!("Computing fib(40)");
+        fib(40);
+        log::info!("Done computing fib(40)");
+    });
+}
+
+async fn run() {
+    tokio::join!(
+        sleeper(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader(),
+        reader()
+    );
+}
+
+#[tokio::main]
+async fn main() {
     simple_logger::init_with_level(Level::Info).unwrap();
 
-    let rt = tokio::runtime::Runtime::new().unwrap();
+    let start = std::time::Instant::now();
+    run().await;
+    let end = std::time::Instant::now();
 
-    let future = run();
-
-    rt.block_on(future);
+    println!("Took {:?} seconds", end - start);
 }
